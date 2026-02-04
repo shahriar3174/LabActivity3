97db0ad96ab315ca02005420cf7510ec3ce8f5d8
shahriar3174 <165700935+shahriar3174@users.noreply.github.com>
2026-02-04 16:23:05 -0500

    Feature Added

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -8,21 +8,28 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    // Declare view properties - the first one is done for you
+    // Declare view properties
     lateinit var displayTextView: TextView
+    lateinit var nameEditText: EditText // Declare this to make code cleaner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialize with views defined in Layout - the first one is done for you
+        // Initialize views
         displayTextView = findViewById(R.id.displayTextView)
+        nameEditText = findViewById(R.id.nameEditText)
+        val clickMeButton = findViewById<Button>(R.id.clickMeButton)
 
-        
-        findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+        clickMeButton.setOnClickListener {
+            val nameInput = nameEditText.text.toString()
+
+            // Requirement 3.1: Change behavior to show error if text is empty
+            if (nameInput.trim().isEmpty()) {
+                displayTextView.text = "Error: Please enter your name!"
+            } else {
+                displayTextView.text = "Hello, $nameInput!"
+            }
         }
-
-
     }
 }
